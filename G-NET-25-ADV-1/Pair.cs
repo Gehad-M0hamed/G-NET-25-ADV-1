0670073262f3024d78ac85151459dad2667877df
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-05-09 15:00:47 +0300

    Q3

---
+++ G-NET-25-ADV-1/Pair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_25_ADV_1
+{
+    public class Pair<T1, T2>
+    {
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override string ToString() => $"{First}:{Second}";
+
+
+    }
+}
--- G-NET-25-ADV-1/Program.cs
+++ G-NET-25-ADV-1/Program.cs
@@ -16,15 +16,28 @@
             #endregion
 
             #region Q2:
-            Container<int> container = new Container<int>();
-            container.Add(10);
-            container.Add(40);
-            container.Add(14);
-            for (int i = 0; i < container._count; i++)
-            {
-                Console.WriteLine($"Element at Container {i}: {container.Get(i)}");
-            }
+            //Container<int> container = new Container<int>();
+            //container.Add(10);
+            //container.Add(40);
+            //container.Add(14);
+            //for (int i = 0; i < container._count; i++)
+            //{
+            //    Console.WriteLine($"Element at Container {i}: {container.Get(i)}");
+            //}
             #endregion
+
+            #region Q3:
+            //Multiple Type Parameters:
+            // Generic classes and methods are not limited to a single placeholder like T.
+            // You can define them with multiple type parameters to handle different types of data simultaneously within the same class.
+            //Pair<int,string> Pair01 = new Pair<int,string> (1, "One");
+            //Pair<string,bool> Pair02 = new Pair<string,bool>("Open?",true);
+            //Console.WriteLine($"Pair01:{Pair01}");
+            //Console.WriteLine($"Pair02:{Pair02}");
+            #endregion
+
+
+
         }
     }
 }
